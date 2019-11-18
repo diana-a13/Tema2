3795e00c2fec55d3521087875435fdea910e9ce7
Ubuntu <ubuntu@ip-172-31-19-21.us-east-2.compute.internal>
2019-11-18 11:55:23 +0000

    first commit

--- main/app.js
+++ main/app.js
@@ -1,6 +1,32 @@
 function addTokens(input, tokens){
+ 
+ 
+    if (typeof input !== "string")
+     throw new Error('Invalid input'); 
+  
+    if(input.length<6 ){
+   throw new Error('Input should have at least 6 characters');
+    }
     
-}
+    for(var i =0; i<tokens.length;i++)
+      {
+       if(typeof tokens[i].tokenName!=="string")
+     {
+        throw ("Invalid array format");
+     }
+   }
+    
+   if(input.includes("...")){
+    for(var i=0;i<tokens.length;i++){
+     input=input.replace("...", "${"+tokens[i].tokenName+ "}");
+     return input;
+    }
+   }
+   else return input;
+ 
+     return input
+   }
+
 
 const app = {
     addTokens: addTokens
